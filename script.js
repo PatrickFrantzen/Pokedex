ba264c6e6bc0602b75583a16d0f196fa8a9223b0
Patrick Frantzen <patrick.frantzen@web.de>
2022-08-22 23:12:02 +0200

    Erste Funktionen eingefügt

---
+++ script.js
@@ -0,0 +1,36 @@
+let currentPokemon;
+
+async function loadPokemon() {
+    let url= 'https://pokeapi.co/api/v2/pokemon/charmander'
+    response = await fetch(url);
+    let currentPokemon = await response.json();
+    console.log(currentPokemon);
+
+    renderPokemonInfo(currentPokemon);
+}
+
+function renderPokemonInfo(currentPokemon) {
+    document.getElementById('pokemon-name').innerHTML = loadName(currentPokemon);
+    document.getElementById('pokemon-pic').src = currentPokemon.sprites.front_default;
+    renderStats(currentPokemon);
+}
+
+function loadName(currentPokemon) {
+    return `
+    # ${currentPokemon['order']} ${currentPokemon['name']};
+    `
+}
+
+function renderStats(currentPokemon) {
+    for (let i = 0; i < currentPokemon['stats'].length; i++) {
+        let stats = currentPokemon['stats'][i];
+        document.getElementById('pokemon-stats').innerHTML += loadStats(stats);
+
+    }
+}
+
+function loadStats(stats) {
+    return `
+    <div>${stats.stat.name}</div> <div>${stats.base_stat}</div>
+    `
+}
